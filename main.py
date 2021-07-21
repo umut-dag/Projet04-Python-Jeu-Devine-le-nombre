9e93b43565df4c09164df62c10351071e0575f79
umut-dag <mlmoracle@yahoo.fr>
2021-07-21 04:51:11 +0200

    Ajout de quelques commentaires

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from random import randint
 # Génération d'un nombre aléatoire compris entre 1 et 100
 solution = randint(1, 100)
 
-# Initialisation des variables
+# Initialisation des variables pour les while qui vont suivre
 nombreEssais = 0
 proposition = 0
 difficulte=""
 
-print("Quel est votre prénom ?")
-name = input()
+name = input("Quel est votre prénom ? ")
 
+# Première façon d'inclure des variables dans les print ( ..." + var + ".... )
 print("Bonjour " + name + ", devinez le nombre entre 1 et 100, vous avez 10 essais.")
 
 while (difficulte != "oui" and difficulte != "non"):
     difficulte = input("Souhaitez-vous augmenter la difficulté en diminuant le nombre d'essais ? taper oui ou non ")
 
 if difficulte == "oui":
-    while (nombreEssais < 1  or nombreEssais > 7):
-        nombreEssais = int(input("Choisissez un nombre d'essai entre 1 et 7 : "))
+    while (nombreEssais < 1  or nombreEssais > 10):
+        nombreEssais = int(input("Choisissez un nombre d'essai entre 1 et 10 : "))
 else :
     nombreEssais = 10
 
@@ -30,13 +30,12 @@ print("Le jeu commence...")
 for i in range(nombreEssais):
 
     while (proposition < 1  or proposition > 100):
+        # Seconde façon d'inclure des variables dans les print ( {} puis ...".format(var)) )
         proposition = int(input("Entrez un nombre (essai {}): ".format(i + 1)))
 
     if proposition < solution:
-        # print("Le nombre a deviner est plus grand que {}".format(essai))
         print("↑ Plus ↑")
     elif proposition > solution:
-        # print("Le nombre a deviner est plus petit que {}".format(essai))
         print("↓ Moins ↓")
     elif proposition == solution:
         if i + 1 < 5:
@@ -49,7 +48,7 @@ for i in range(nombreEssais):
             print("==================================================================")
         break
 
-    # Je force cette var à zero afin d'invalider le while de la ligne 32 et lui permettre la vérification (1-100) de la prochaine poposition
+    # Je force cette var à zero afin d'invalider le while de la ligne 32 et lui permettre la vérification (proposition entre 1 et 100) lors de la prochaine poposition
     proposition = 0
 
 # Le joueur n'a pas trouvé le nombre à deviner...
